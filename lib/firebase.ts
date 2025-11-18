a58ad9407fc6fe448c7949af38d845a43fec69b5
Gary Legenda <garylegenda@gmail.com>
2025-11-17 23:30:35 -0500

    Fix: correct Firebase API key env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,7 +4,7 @@ import { getAuth, Auth } from 'firebase/auth';
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
